318fabab87952057e895e4a8f3d6a3e93f374653
swastikjhacoder <mr.swastikjha@gmail.com>
2023-04-29 13:17:01 +0530

    showing posts and comments in home page

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,12 @@
-module.exports.post = function(req, res){
-    res.end('<h1>User Posts</h1>');
+const Post = require('../models/post')
+
+module.exports.create = function(req, res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+    }, function(err, post){
+        if(err){console.log('error in creating a post'); return;}
+
+        return res.redirect('back');
+    });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-const usersController = require('../controllers/posts_controller');
+const postsController = require('../controllers/posts_controller');
 
-router.get('/post', usersController.post);
+router.post('/create', passport.checkAuthentication, postsController.create);
+
 
 module.exports = router;
