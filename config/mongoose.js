26a60ed0aa2b435b5a5b0f67919dcca366ec430e
swastikjhacoder <mr.swastikjha@gmail.com>
2023-04-29 10:35:06 +0530

    passport authenticate

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/codial_development');
+mongoose.connect('mongodb://localhost/codeial_development');
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console,"Error connecting to MongoDB"));
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
 
 db.once('open', function(){
-    console.log('Connected to the database :: MongoDB');
+    console.log('Connected to Database :: MongoDB');
 });
+
 
 module.exports = db;
--- models/user.js
+++ models/user.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
