f14f525e04da7001e26afae5a1c385f2d41125d9
swastikjhacoder <mr.swastikjha@gmail.com>
2023-04-29 13:41:14 +0530

    flash messages implemented

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,41 +1,54 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.body.post);
 
         if (post){
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
-                // handle error
-
-                post.comments.push(comment);
-                post.save();
-
-                res.redirect('/');
             });
-        }
 
-    });
+            post.comments.push(comment);
+            post.save();
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
+        }
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
+    
 }
 
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
+module.exports.destroy = async function(req, res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
         if (comment.user == req.user.id){
 
             let postId = comment.post;
 
             comment.remove();
 
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
-                return res.redirect('back');
-            })
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
         }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
+    
 }
