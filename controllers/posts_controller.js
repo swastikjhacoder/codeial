47481280f912dd671a3a3d3a87b4468d3e7cae34
swastikjhacoder <mr.swastikjha@gmail.com>
2023-04-29 14:08:09 +0530

    codeial implemented

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,16 +3,30 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
         
+        if (req.xhr){
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
+
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
         req.flash('success', 'Post published!');
         return res.redirect('back');
 
     }catch(err){
         req.flash('error', err);
+        // added this to view the error on console as well
+        console.log(err);
         return res.redirect('back');
     }
   
@@ -28,6 +42,16 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
+
+
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
 
             req.flash('success', 'Post and associated comments deleted!');
 
